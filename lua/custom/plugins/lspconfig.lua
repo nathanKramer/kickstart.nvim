0cd1cd7b35f994f5039d9de3347b029247401dd0
Nathan Kramer <nathan.l.kramer@gmail.com>
2024-03-26 21:27:54 +1100

    Add typescript

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -152,7 +152,7 @@ return {
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- tsserver = {},
+      tsserver = {},
       --
       elixirls = {},
       elmls = {},
