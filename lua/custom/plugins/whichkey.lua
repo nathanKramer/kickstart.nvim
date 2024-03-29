3acec193814593a2079284c729365e0eb9912c0c
Nathan Kramer <nathan.l.kramer@gmail.com>
2024-03-29 21:26:12 +1100

    Minor tweaks

--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -25,7 +25,7 @@ return {
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      ['<leader>p'] = { name = '[P]roject', _ = 'which_key_ignore' },
     }
   end,
 }
--- lua/kickstart/remap.lua
+++ lua/kickstart/remap.lua
@@ -8,6 +8,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>', { desc = '[C]opilot [E]nable', noremap = true, silent = true })
 vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = '[C]opilot [D]isable', noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = '[W]rite' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
