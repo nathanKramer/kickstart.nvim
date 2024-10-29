ddab713d2f23699db8a7ccb9290fd3d27540b8c9
Nathan Kramer <nathan.l.kramer@gmail.com>
2024-10-29 17:03:31 +1100

    Fix which key mappings

--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -16,16 +16,13 @@
 return {
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
-
-    -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>p'] = { name = '[P]roject', _ = 'which_key_ignore' },
-    }
-  end,
+  opts = {
+    spec = {
+      { "<leader>c", group = "[C]ode" },
+      { "<leader>d", group = "[D]ocument" },
+      { "<leader>p", group = "[P]roject" },
+      { "<leader>r", group = "[R]ename" },
+      { "<leader>s", group = "[S]earch" },
+    },
+  },
 }
