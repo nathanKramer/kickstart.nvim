9318c53b1478fc35405d4d230e018686dacebaa6
Nathan Kramer <nathan.l.kramer@gmail.com>
2025-07-17 19:48:33 +1000

    Drop markdownlint

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -1,8 +1,8 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = { -- Automatically install LSPs and related tools to stdpath for Neovim
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
+    'mason-org/mason.nvim',
+    'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
 
     -- Useful status updates for LSP.
