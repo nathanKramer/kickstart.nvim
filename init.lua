203d574c754a9b0b5abf055e4c4cd9a465d2c4c6
Nathan Kramer <nathan.l.kramer@gmail.com>
2025-07-20 19:51:17 +1000

    Add solargraph and nerd font

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
 
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 require 'kickstart.set'
 require 'kickstart.remap'
--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -146,6 +146,7 @@ return {
       gopls = {},
       pyright = {},
       rust_analyzer = {},
+      solargraph = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
